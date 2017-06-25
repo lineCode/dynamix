7e63a598427dacc73841399d2a659dcb6edf95a0
Borislav Stanimirov <b.stanimirov@abv.bg>
2017-06-26 00:04:50 +0300

    Small step towards gcc/clang percompiled headers. Still no --std flag support

--- build/macros/precompiled_header.cmake
+++ build/macros/precompiled_header.cmake
@@ -50,7 +50,7 @@ macro(add_precompiled_header TARGET_NAME PRECOMPILED_HEADER PRECOMPILED_SOURCE)
                     # Set and automatically include precompiled header
                     set_source_files_properties(
                         ${SOURCE_FILE}
-                        PROPERTI
+                        PROPERTIES
                         COMPILE_FLAGS "/Yu\"${PRECOMPILED_HEADER_NAME}\" /Fp\"${PRECOMPILED_BINARY}\" /FI\"${PRECOMPILED_HEADER_NAME}\""
                         OBJECT_DEPENDS "${PRECOMPILED_BINARY}")
                 endif()
@@ -72,27 +72,37 @@ macro(add_precompiled_header TARGET_NAME PRECOMPILED_HEADER PRECOMPILED_SOURCE)
         make_directory(${OUTPUT_DIR})
         set(OUTPUT_NAME "${OUTPUT_DIR}/${PRECOMPILED_HEADER_NAME}.gch")
 
-        # Gather compiler options, definitions, etc.
-        string(TOUPPER "CMAKE_CXX_FLAGS_${CMAKE_BUILD_TYPE}" CXX_FLAGS)
-        set(COMPILER_FLAGS "${${CXX_FLAGS}} ${CMAKE_CXX_FLAGS}")
-        get_directory_property(DIRECTORY_FLAGS INCLUDE_DIRECTORIES)
-        foreach(item ${DIRECTORY_FLAGS})
-            list(APPEND COMPILER_FLAGS "-I${item}")
-        endforeach(item)
-        get_directory_property(DIRECTORY_FLAGS COMPILE_DEFINITIONS)
-        foreach(item ${DIRECTORY_FLAGS})
-            list(APPEND COMPILER_FLAGS "-D${item}")
-        endforeach(item)
+	# Export compiler flags via a generator to a response file
+        set(PCH_FLAGS_FILE "${OUTPUT_DIR}/${PRECOMPILED_HEADER_NAME}.rsp")
+        set(_include_directories "$<TARGET_PROPERTY:${TARGET_NAME},INCLUDE_DIRECTORIES>")
+        set(_compile_definitions "$<TARGET_PROPERTY:${TARGET_NAME},COMPILE_DEFINITIONS>")
+        set(_compile_flags "$<TARGET_PROPERTY:${TARGET_NAME},COMPILE_FLAGS>")
+        set(_compile_options "$<TARGET_PROPERTY:${TARGET_NAME},COMPILE_OPTIONS>")
+        set(_include_directories "$<$<BOOL:${_include_directories}>:-I$<JOIN:${_include_directories},\n-I>\n>")
+        set(_compile_definitions "$<$<BOOL:${_compile_definitions}>:-D$<JOIN:${_compile_definitions},\n-D>\n>")
+        set(_compile_flags "$<$<BOOL:${_compile_flags}>:$<JOIN:${_compile_flags},\n>\n>")
+        set(_compile_options "$<$<BOOL:${_compile_options}>:$<JOIN:${_compile_options},\n>\n>")
+        file(GENERATE OUTPUT "${PCH_FLAGS_FILE}" CONTENT "${_compile_definitions}${_include_directories}${_compile_flags}${_compile_options}\n")
 
         # Add a custom target for building the precompiled header.
-        separate_arguments(COMPILER_FLAGS)
         add_custom_command(
             OUTPUT ${OUTPUT_NAME}
-            COMMAND ${CMAKE_CXX_COMPILER} ${COMPILER_FLAGS} -x c++-header -o ${OUTPUT_NAME} ${PRECOMPILED_HEADER}
+            COMMAND ${CMAKE_CXX_COMPILER} @${PCH_FLAGS_FILE} -x c++-header -o ${OUTPUT_NAME} ${PRECOMPILED_HEADER}
             DEPENDS ${PRECOMPILED_HEADER})
         add_custom_target(${TARGET_NAME}_gch DEPENDS ${OUTPUT_NAME})
         add_dependencies(${TARGET_NAME} ${TARGET_NAME}_gch)
-        set_target_properties(${TARGET_NAME} PROPERTIES COMPILE_FLAGS "-include ${PRECOMPILED_HEADER_NAME} -Winvalid-pch")
+
+        # set_target_properties(${TARGET_NAME} PROPERTIES COMPILE_FLAGS "-include ${PRECOMPILED_HEADER_NAME} -Winvalid-pch")
+        get_target_property(SOURCE_FILES ${TARGET_NAME} SOURCES)
+        foreach(SOURCE_FILE ${SOURCE_FILES})
+            set(PCH_COMPILE_FLAGS "")
+            if(SOURCE_FILE MATCHES \\.\(c|cc|cxx|cpp\)$)
+                get_source_file_property(SRC_COMPILE_FLAGS "${SOURCE_FILE}" COMPILE_FLAGS)
+                set_source_files_properties(${SOURCE_FILE} PROPERTIES
+                   COMPILE_FLAGS "${SRC_COMPILE_FLAGS} -include ${PRECOMPILED_HEADER_NAME} -Winvalid-pch"
+                )
+            endif()
+        endforeach()
     else()
         message(FATAL_ERROR "Unknown generator for add_precompiled_header.")
     endif()
